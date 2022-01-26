93374e734a6ee658139d7a95eda74f6e81c3ff91
Kep <KepCreations@gmail.com>
2022-01-26 16:51:48 -0500

    Add edit and delete icons to PostDisplay

--- src/components/PostDisplay.js
+++ src/components/PostDisplay.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './PostDisplay.css';
 
-const PostDisplay = ({ post }) => {
+const PostDisplay = ({ post, toggleEdit, deletePost }) => {
 	console.debug('PostDisplay');
 
 	const { title, description, body } = post;
 
 	return (
-		<div className="Post">
+		<div className="PostDisplay">
 			<div>
 				<h2>Post Display</h2>
 				<h2>{title}</h2>
@@ -15,6 +15,12 @@ const PostDisplay = ({ post }) => {
 					<em>{description}</em>
 				</p>
 				<div>{body}</div>
+			</div>
+			<div className="PostDisplay-right">
+				<div className="PostDisplay-edit-area">
+					<i className="fas fa-edit text-primary" onClick={toggleEdit} />
+					<i className="fas fa-times text-danger" onClick={deletePost} />
+				</div>
 			</div>
 		</div>
 	);
