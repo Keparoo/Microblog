3b309e3b3a134ec2a4f08a3fa3fd4dc4a56a7091
Kep <KepCreations@gmail.com>
2022-01-31 15:48:28 -0500

    Update App.test.js

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import { createReduxStore } from 'redux';
 
-test('renders learn react link', () => {
-	render(<App />);
-	const linkElement = screen.getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+// 	render(<App />);
+// 	const linkElement = screen.getByText(/learn react/i);
+// 	expect(linkElement).toBeInTheDocument();
+// });
+
+it('renders without crashing', () => {
+	const renderApp = (store = createReduxStore()) => {
+		render(
+			<Provider store={store}>
+				<MemoryRouter>
+					<App />
+				</MemoryRouter>
+			</Provider>
+		);
+	};
+});
+
+it('matches snapshot', () => {
+	const renderApp = (store = createReduxStore()) => {
+		const { asFragment } = render(
+			<Provider store={store}>
+				<MemoryRouter>
+					<App />
+				</MemoryRouter>
+			</Provider>
+		);
+		expect(asFragment()).toMatchSnapshot();
+	};
 });
